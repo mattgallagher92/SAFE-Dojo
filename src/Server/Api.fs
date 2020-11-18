b7906998ceb55aa9a78a2cb81a9e515f9674cb1c
Matt <mjg1992@hotmail.co.uk>
2020-11-18 19:30:26 +0000

    Make changes for task 1.3

--- src/Server/Api.fs
+++ src/Server/Api.fs
@@ -46,10 +46,7 @@ let getWeather postcode = async {
 
 let dojoApi =
     { GetDistance = getDistanceFromLondon
-
-      (* Task 1.0 CRIME: Bind the getCrimeReport function to the GetCrimes method to
-         return crime data. Use the above GetDistance field as an example. *)
-      GetCrimes = fun postcode -> async { return Array.empty }
+      GetCrimes = getCrimeReport
 
       (* Task 4.2 WEATHER: Hook up the weather endpoint to the getWeather function. *)
     }
